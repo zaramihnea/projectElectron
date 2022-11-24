48e8867389380702b3d54724c95a1f4ec606599c
Zara Mihnea <zaramihnea@icloud.com>
2022-11-24 13:40:47 +0200

    ok

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 //Electron is an open source project for the development of a 2D graphics circuit simulator
 //Project created by Bursuc Eduarda & Zara Mihnea-Tudor, Faculty of Computer Sience, University "Alexandru Ioan Cuza" of Iasi, Romania, 2022
 
+
 /*<--Begin libraries-->*/
 #include <iostream>
 #include <fstream>
